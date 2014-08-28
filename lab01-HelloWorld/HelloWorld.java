e632a903d853fde905adb72ebcf2e4e690cd5243
rlm215 <rlm215@lehigh.edu>
2014-08-28 00:36:59 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,27 @@
+//////////////////////////////////////////////////////////////////////////
+//Rachel Mahoney
+//Lab 1
+//Hello World Java Program
+//
+//  first compile the program
+//  then run the program
+
+// need to define a class
+public class HelloWorld{
+
+//  need to add a main method
+  public static void main(String[] args)  {
+      
+// then print the statement that has a box around it
+    System.out.println("#######################");
+    System.out.println("#######################");
+    System.out.println("#                     #");
+    System.out.println("#  Hello World 2014!! #");
+    System.out.println("#                     #");
+    System.out.println("#######################");
+    System.out.println("#######################");
+    System.out.println("test");
+    
+    //Test the code
+  }
+}
