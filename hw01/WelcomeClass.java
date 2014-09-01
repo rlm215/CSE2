31cc57fa336883a8f9a8da9d80dfaf0c612f07eb
rlm215 <rlm215@lehigh.edu>
2014-09-01 20:52:59 +0000

    Updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,26 @@
+//Rachel Mahoney
+//hw01
+//Welcome Class Java Program
+
+//define a class
+public class WelcomeClass{
+    
+//Add the method 
+    public static void main(String[] args){
+
+//Print statment below with network ID and short autobiography
+    System.out.println("-----------"     );
+    System.out.println("| WELCOME |"     );
+    System.out.println("-----------"     );
+    System.out.println("  ^   ^   ^   ^   ^   ^");
+    System.out.println(" / \\ / \\ / \\ / \\ / \\ / \\");
+    System.out.println("|-R --L --M --2 --1-- 5 -|");
+    System.out.println(" \\ / \\ / \\ / \\ / \\ / \\ /");
+    System.out.println("  v   v   v   v   v   v");
+    System.out.println("I am a senior Industrial Engineering major.");
+    System.out.println("I went parasailing in May.");
+    
+        }
+}
+    //Test the program
+    
