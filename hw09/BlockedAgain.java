7c5e118495d0492eceea8cd5fee28c2bf586658b
rlm215 <rlm215@lehigh.edu>
2014-11-04 22:18:33 +0000

    updated BlockedAgain

---
+++ hw09/BlockedAgain.java
@@ -0,0 +1,87 @@
+//Rachel Mahoney
+//hw 10
+//2 Nov 2014
+//learning how to print nested sequence of method calls
+//asks user for input then check for int and range
+
+import java.util.Scanner;
+public class BlockedAgain {
+    public static void main(String[] s) {
+        int m;
+        //force user to enter int in range 1-9, inclusive.
+        m = getInt();
+        allBlocks(m);
+    }
+
+
+    public static int getInt() {
+        System.out.println("Enter an int between 1 and 9 inclusive.");
+        Scanner scan;
+        scan = new Scanner(System.in);
+        int input = checkInt(scan);
+        input = checkRange(input);
+        return input;
+    }
+    public static int checkInt(Scanner scan) {
+            int a = 0;
+            if (scan.hasNextInt()) {
+                a = scan.nextInt();
+            } //closes if
+            else {
+                System.out.println("You did not enter an int; try again");
+                Scanner in = new Scanner(System.in);
+                a = checkInt( in );
+            } //closes else
+            return a;
+        } //closes checkInt
+
+    public static int checkRange(int b) {
+            if ((b >= 1) & (b <= 9)) {} //closes if
+            else {
+                System.out.println("You did not enter and int in [1,9]; try again.");
+                Scanner call = new Scanner(System.in);
+                b = checkInt(call);
+            } //closes else
+            return b;
+        } //closes checkRange
+
+    public static void allBlocks(int c) {
+            int input = getInt();
+            int across = 1;
+            int down = 1;
+            for (int a = 1; a <= input; a++) {
+                for (c = 0; c < input - 1; c++) {
+                }
+                for (int j = 0; j <= a - 1; j++) {
+                    for (int b = across; b > 0; b--) {
+                        System.out.print(" ");
+                        System.out.print(a);
+                    }
+                    System.out.println("\n ");
+                }
+                
+                for (int b = across; b > 0; b--) {
+                    System.out.print(" ");
+                    System.out.print('-');
+                }
+                
+                System.out.println("\n ");
+                across += 2;
+            }
+            
+            
+
+        } //closes allBlocks
+        /*when I tried to convert my code into the other methods it didn't work.
+        I couldn't get the desired print out with the multiple methods
+        and was in fact worse than the print out I gave in this one method. 
+            
+            public static void block(){ //calls line
+            
+            }//closes block method
+
+            public static int line(int c){
+               
+            }//close line*/
+
+} //closes class
