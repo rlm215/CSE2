9e0bafb3ca1c9933659f7388462986f0136ba31b
rlm215 <rlm215@lehigh.edu>
2014-11-04 22:21:53 +0000

    updated BlockedAgain

--- hw09/BlockedAgain.java
+++ hw09/BlockedAgain.java
@@ -18,11 +18,11 @@ public class BlockedAgain {
         System.out.println("Enter an int between 1 and 9 inclusive.");
         Scanner scan;
         scan = new Scanner(System.in);
-        int input = checkInt(scan);
-        input = checkRange(input);
-        return input;
+        int input = checkInt(scan);//checks that input is int
+        input = checkRange(input);//checks that input is in range
+        return input;//returns the int to main method
     }
-    public static int checkInt(Scanner scan) {
+    public static int checkInt(Scanner scan) { //method to check int
             int a = 0;
             if (scan.hasNextInt()) {
                 a = scan.nextInt();
@@ -30,23 +30,24 @@ public class BlockedAgain {
             else {
                 System.out.println("You did not enter an int; try again");
                 Scanner in = new Scanner(System.in);
-                a = checkInt( in );
+                a = checkInt( in ); //recalls scanner
             } //closes else
             return a;
         } //closes checkInt
 
     public static int checkRange(int b) {
-            if ((b >= 1) & (b <= 9)) {} //closes if
+            if ((b >= 1) & (b <= 9)) {} //if doesn't need statement
+            //automatically returns b
             else {
                 System.out.println("You did not enter and int in [1,9]; try again.");
                 Scanner call = new Scanner(System.in);
-                b = checkInt(call);
+                b = checkInt(call); //recalls scanner
             } //closes else
             return b;
         } //closes checkRange
 
     public static void allBlocks(int c) {
-            int input = getInt();
+            int input = getInt(); //calls the int input from getInt
             int across = 1;
             int down = 1;
             for (int a = 1; a <= input; a++) {
